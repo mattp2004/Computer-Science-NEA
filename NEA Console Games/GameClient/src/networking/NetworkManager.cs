da6def18454918ffe3a6730af35655ba56a54540
dcdb <79600718+dcdbee@users.noreply.github.com>
2022-09-23 13:24:48 +0100

    b
    b

--- NEA Console Games/GameClient/src/networking/NetworkManager.cs
+++ NEA Console Games/GameClient/src/networking/NetworkManager.cs
@@ -10,7 +10,7 @@ namespace GameClient.src.networking
     {
         public static void QuickPlay()
         {
-            NetworkClient test = new NetworkClient("localhost", 6000);
+            NetworkClient test = new NetworkClient("38.242.132.154", 6000);
             test.Connect();
         }
     }

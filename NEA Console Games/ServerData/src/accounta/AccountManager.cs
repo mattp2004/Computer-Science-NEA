c26e735b76b97343fbe93ae9b61a2524198081c7
dcdb <79600718+dcdbee@users.noreply.github.com>
2022-11-06 19:19:28 +0000

    FIX
    FIX

---
+++ NEA Console Games/ServerData/src/accounta/AccountManager.cs
@@ -0,0 +1,36 @@
+﻿using ServerData.src.data;
+using ServerData.src.misc;
+using ServerData.src.network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerData.src.account
+{
+    class AccountManager
+    {
+
+        public static Account FetchAccount(Client _client)
+        {
+            if (Account.GetCache().ContainsKey(_client))
+            {
+                return Account.GetCache()[_client];
+            }
+            else
+            {
+                DataManager.instance.accountRepository.LoadAccount(_client);
+                try
+                {
+                    return Account.GetCache()[_client];
+                }
+                catch (Exception e)
+                {
+                    Util.Error(e);
+                }
+            }
+            return null;
+        }
+    }
+}
